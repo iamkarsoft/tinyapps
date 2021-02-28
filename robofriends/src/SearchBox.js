c816d8926f47cd31a47b92b9c7c2f1099c9f0b5b
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-02-28 20:24:21 +0000

    [feature] create search box component

---
+++ robofriends/src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () =>{
+	return(
+		<div class="w-full">
+			<input type="search" placehodler="searchbox"  className="text-center px-4 py-2 border-2 rounded-lg"/>
+		</div>
+		);
+}
+
+export default SearchBox;
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import CardList from './CardList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {robots} from './robots';
 ReactDOM.render(
   <React.StrictMode>
- 	<CardList robots={robots} />
+ 	<App />
   </React.StrictMode>,
   document.getElementById('root')
 );
